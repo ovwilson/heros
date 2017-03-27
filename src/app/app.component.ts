aed189b20fb70d5321830e24a343ecf8ff8f4b61
landough <orlando_wilson@msn.com>
2017-03-27 00:54:41 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,17 @@ import { Hero } from "./models/hero";
 import { LISTEN_TO_HEROES, HEROES_FILTER_FRIENDS } from "./store/actions/actions";
 import "../../public/styles.css";
 
+declare var firebase: any;
+
+// Initialize Firebase
+const config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messageSenderId
+};
+
 @Component({
     selector: "body",
     templateUrl: "./app.component.html",
@@ -13,14 +24,14 @@ import "../../public/styles.css";
 export class AppComponent implements OnInit {
 
     title: string = "Tour of Heroes";
-    hero: Hero = new Hero("","","",false);
-   
-    constructor(private store: Store<any>) { }
+    hero: Hero = new Hero("", "", "", false);
+
+    constructor(private store: Store<any>) {
+        firebase.initializeApp(config);
+    }
 
     ngOnInit() {
         this.store.dispatch({ type: LISTEN_TO_HEROES });
-        this.store.dispatch({ type: HEROES_FILTER_FRIENDS, payload:{list:[2,4,3,0,2,2], userList:[0,4,3,2,3]} });    
+        this.store.dispatch({ type: HEROES_FILTER_FRIENDS, payload: { list: [2, 4, 3, 0, 2, 2], userList: [0, 4, 3, 2, 3] } });
     }
-
-
 }
